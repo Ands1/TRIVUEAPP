4cf3f2eb113bfefa45d15b37073bef94a242558d
Ands <1azands@gmail.com>
2021-09-18 13:39:53 -0600

    Working but POST not

--- app/controllers/quiz/takesurveys_controller.rb
+++ app/controllers/quiz/takesurveys_controller.rb
@@ -1,6 +1,7 @@
 class Quiz::TakesurveysController < ApplicationController
 
     def index
+        @quiz = Quiz.find_by_id(params[:quiz_id])
         @takes = Quiz::Question.where(quizzes_id: params[:quiz_id])
     end
 
@@ -9,7 +10,7 @@ class Quiz::TakesurveysController < ApplicationController
         puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", params, @status_answer
         respond_to do |format|
             
-            format.html { redirect_to @quiz, notice: "PREGUNTAS GUARDADAS" }
+            format.html { redirect_to quiz_takesurveys_url, notice: "PREGUNTAS GUARDADAS" }
         end
     end
 end
